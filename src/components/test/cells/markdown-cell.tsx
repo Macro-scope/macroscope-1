33ba54ff0c120ce167fa94c67916a1e592b3dcaf
Aditya Yaduvanshi <aditya97y@gmail.com>
2025-01-03 17:04:38 +0530

    fix

--- src/components/test/cells/markdown-cell.tsx
+++ src/components/test/cells/markdown-cell.tsx
@@ -1,55 +1,55 @@
-/* eslint-disable react/display-name */
-import {
-  drawTextCell,
-  GridCellKind,
-  MarkdownCell,
-} from '@glideapps/glide-data-grid';
-import * as React from 'react';
-import { prepTextCell } from '../lib';
+// /* eslint-disable react/display-name */
+// import {
+//   drawTextCell,
+//   GridCellKind,
+//   MarkdownCell,
+// } from '@glideapps/glide-data-grid';
+// import * as React from 'react';
+// import { prepTextCell } from '../lib';
 
-export const markdownCellRenderer: InternalCellRenderer<MarkdownCell> = {
-  getAccessibilityString: (c) => c.data?.toString() ?? '',
-  kind: GridCellKind.Markdown,
-  needsHover: false,
-  needsHoverPosition: false,
-  drawPrep: prepTextCell,
-  measure: (ctx: any, cell: any, t: any) => {
-    const firstLine = cell.data.split('\n')[0];
-    return ctx.measureText(firstLine).width + 2 * t.cellHorizontalPadding;
-  },
-  draw: (a: any) => drawTextCell(a, a.cell.data, a.cell.contentAlign),
-  onDelete: (c: any) => ({
-    ...c,
-    data: '',
-  }),
-  provideEditor: () => (p) => {
-    const {
-      onChange,
-      value,
-      target,
-      onFinishedEditing,
-      markdownDivCreateNode,
-      forceEditMode,
-      validatedSelection,
-    } = p;
-    return (
-      // <MarkdownOverlayEditor
-      //   onFinish={onFinishedEditing}
-      //   targetRect={target}
-      //   value={value}
-      //   validatedSelection={validatedSelection}
-      //   onChange={(e) =>
-      //     onChange({
-      //       ...value,
-      //       data: e.target.value,
-      //     })
-      //   }
-      //   forceEditMode={forceEditMode}
-      //   createNode={markdownDivCreateNode}
-      // />
-      <></>
-    );
-  },
-  onPaste: (toPaste: any, cell: any) =>
-    toPaste === cell.data ? undefined : { ...cell, data: toPaste },
-};
+// export const markdownCellRenderer: InternalCellRenderer<MarkdownCell> = {
+//   getAccessibilityString: (c) => c.data?.toString() ?? '',
+//   kind: GridCellKind.Markdown,
+//   needsHover: false,
+//   needsHoverPosition: false,
+//   drawPrep: prepTextCell,
+//   measure: (ctx: any, cell: any, t: any) => {
+//     const firstLine = cell.data.split('\n')[0];
+//     return ctx.measureText(firstLine).width + 2 * t.cellHorizontalPadding;
+//   },
+//   draw: (a: any) => drawTextCell(a, a.cell.data, a.cell.contentAlign),
+//   onDelete: (c: any) => ({
+//     ...c,
+//     data: '',
+//   }),
+//   provideEditor: () => (p) => {
+//     const {
+//       onChange,
+//       value,
+//       target,
+//       onFinishedEditing,
+//       markdownDivCreateNode,
+//       forceEditMode,
+//       validatedSelection,
+//     } = p;
+//     return (
+//       // <MarkdownOverlayEditor
+//       //   onFinish={onFinishedEditing}
+//       //   targetRect={target}
+//       //   value={value}
+//       //   validatedSelection={validatedSelection}
+//       //   onChange={(e) =>
+//       //     onChange({
+//       //       ...value,
+//       //       data: e.target.value,
+//       //     })
+//       //   }
+//       //   forceEditMode={forceEditMode}
+//       //   createNode={markdownDivCreateNode}
+//       // />
+//       <></>
+//     );
+//   },
+//   onPaste: (toPaste: any, cell: any) =>
+//     toPaste === cell.data ? undefined : { ...cell, data: toPaste },
+// };
